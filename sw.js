d87ff9063c9a10c08948c3f6026903199bfcf280
Tafajjul Ansari <tafajjulans141@gmail.com>
2025-08-20 10:28:37 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,22 @@
-const CACHE_NAME = "mess-tracker-v1";
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./style.css",
-  "./stats.html",
-  "./table.html"
+const CACHE_NAME = "mess-tracker-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
 ];
 
 // Install
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 // Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
-        
